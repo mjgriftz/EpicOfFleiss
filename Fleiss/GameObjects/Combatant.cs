f2bcc7854a18df28e508530355de8cd15bdda92c
mjgriftz <marcusjgriffiths@gmail.com>
2018-06-12 02:29:53 -0700

    Added Combatant and Item

---
+++ Fleiss/GameObjects/Combatant.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Fleiss.GameObjects
+{
+    public enum Race
+    {
+        //todo
+        Hooman, 
+        Cat, 
+        Doggo
+    };
+    public abstract class Combatant
+    {
+        public CombatProfile Profile { get; protected set; }
+        public CombatStatus Status { get; protected set; }
+        public Equipment Equip { get; protected set; }
+
+        public Combatant(CombatProfile cp, CombatStatus cs, Equipment eq)
+        {
+            this.Profile = cp;
+            this.Status = cs;
+            this.Equip = eq;
+        }
+        public class CombatProfile
+        {
+            public string Name { get; private set; }
+            public string Origin { get; private set; }
+            public string Description { get; private set;  }
+            public Race Race { get; private set;  }
+
+
+        }
+        public class CombatStatus
+        {
+            //todo
+        }
+        public class Equipment
+        {
+            //todo
+        }
+    }
+
+    public class Ally: Combatant
+    {
+        //todo
+    }
+    public class Enemy: Combatant
+    {
+        //todo
+    }
+
+}
---
+++ Fleiss/GameObjects/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Fleiss.GameObjects
+{
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public Item(string name, string description)
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+    }
+    public abstract class Consumable: Item
+    {
+        //todo
+    }
+    public abstract class Equipment: Item
+    {
+        //todo
+    }
+    public class KeyItem: Item
+    {
+        //todo
+    }
+}
