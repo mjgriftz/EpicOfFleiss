d5c24362ca6f619eb9ba93ef9f87fc9cd04968cc
mjgriftz <marcusjgriffiths@gmail.com>
2018-06-27 11:12:33 -0700

    Added window behavior

--- Fleiss/Engine/RenderEngine.cs
+++ Fleiss/Engine/RenderEngine.cs
@@ -3,28 +3,66 @@ namespace Fleiss.Engine
 {
     public class RenderEngine
     {
-        public GameView rootView;
+        public GameWindow rootWindow;
 
         public RenderEngine()
         {
-            //TODO
+            rootWindow = GameWindow.MakeTextWindow();
         }
 
         public static RenderEngine DefaultWindow()
         {
             //Update 
+
             return new RenderEngine();
         }
 
         public void DrawWindow()
         {
-            //TODO
+            this.rootWindow.Draw();
+        }
+    }
+    public abstract class GameWindow
+    {
+        public virtual void Draw() { }
+        public static TextWindow MakeTextWindow()
+        {
+            return new TextWindow();
+        }
+    }
+
+    public class TextWindow : GameWindow
+    {
+
+        protected char[,] drawData_;
+        protected int width_;
+        protected int height_;
+        public TextWindow() : this(80, 24) { }
+
+        public TextWindow(int height, int width)
+        {
+            //initialize the drawData_ buffer
+            drawData_ = new char[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    drawData_[i, j] = ' ';
+                }
+            }
+        }
+
+        public override void Draw()
+        {
+            for (int i = 0; i < width_; i++)
+            {
+                for (int j = 0; j < height_; j++)
+                {
+                    Console.Write(drawData_[i, j]);
+                }
+            }
         }
     }
 }
 
-public class GameView
-{
-    
 
-}
